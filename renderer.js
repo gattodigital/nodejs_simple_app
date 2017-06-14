31cacd29d46820a3bc828eaa21a8d1312aa41541
Gatto Digital <andy@gattodigital.com>
2017-06-13 23:39:34 -0500

    binding values

--- renderer.js
+++ renderer.js
@@ -1,10 +1,20 @@
 var fs = require("fs");
 
+function mergeValues(values, content) {
+	// cycle over keys
+	for(var key in values) {
+		// replace all {{key}} with values from the value object
+		content = content.replace("{{" + key + "}}", values[key]);
+	}
+		// return merged content
+		return content;
+}
+
 function view(templateName, values, response) {
 	// read from template files
-	var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+	var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
 	// insert values in to the content
-
+	fileContents = mergeValues(values, fileContents);
 	// write out the contents to response
 	response.write(fileContents);
 }
